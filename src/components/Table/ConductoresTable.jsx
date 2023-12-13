06b58289cf9c328d2aadce4c98a5c03d89e9d79e
Javvier3 <20203tn021@utez.edu.mx>
2023-12-12 20:08:47 -0600

    Conductores Register and Delete

--- src/components/Table/ConductoresTable.jsx
+++ src/components/Table/ConductoresTable.jsx
@@ -5,13 +5,17 @@ import {
   PlusOutlined,
   EditOutlined,
   DeleteOutlined,
+  EyeOutlined
 } from "@ant-design/icons";
 import "./Table.css";
 import defaultimg from "../../assets/images/default.jpg";
 import { useNavigate } from "react-router-dom";
-import { getAllConductores } from "../../service/conductores/serviceConductores";
+import { deleteConductorById, getAllConductores } from "../../service/conductores/serviceConductores";
+import Swal from "sweetalert2";
 
 const ConductoresTable = () => {
+  const hoy = new Date();
+
   const [conductoresData, setConductoresData] = useState([]);
   const navigate = useNavigate();
 
@@ -30,6 +34,42 @@ const ConductoresTable = () => {
 
   const handleClick = () => {
     navigate("/conductoresEdit");
+  };
+
+  const handleDeleteUnidad = async (id) => {
+    try {
+      const result = await Swal.fire({
+        title: "¿Estás seguro de que deseas eliminar a este conductor?",
+        showDenyButton: true,
+        confirmButtonText: "Sí, eliminar",
+        denyButtonText: "Cancelar",
+      });
+
+      if (result.isConfirmed) {
+        await deleteConductorById(id)
+          .then((response) => {
+            console.log(response)
+            if (response.data.error === false) {
+              Swal.fire("Eliminación exitosa", "", "success")
+                .then(()=>{ window.location.href = "/conductores";})
+            } else {
+              Swal.fire("Error al eliminar la unidad", response.data.message || "", "error")
+              .then(()=>{ window.location.href = "/conductores";})
+            }
+          })
+          .catch((error) => {
+            console.error("Error al eliminar al conductor:", error);
+            Swal.fire("Error al eliminar al conductor", "", "error")
+            .then(()=>{ window.location.href = "/conductores";})
+          });
+      } else if (result.isDenied) {
+        Swal.fire("Eliminación cancelada", "", "info");
+      }
+    } catch (error) {
+      console.error("Error al procesar la eliminación:", error);
+      Swal.fire("Error al procesar la eliminación", "", "error")
+      .then(()=>{ window.location.href = "/conductores";})
+    }
   };
 
   const columns = [
@@ -72,10 +112,11 @@ const ConductoresTable = () => {
     {
       title: "Acciones",
       dataIndex: "acciones",
+      dataIndex: "acciones",
       render: (text, record) => (
         <Space size="middle">
           <Button icon={<EditOutlined />} onClick={handleClick} />
-          <Button icon={<DeleteOutlined />} />
+          <Button icon={<DeleteOutlined/>} onClick={()=>{handleDeleteUnidad(record.key)}} />
         </Space>
       ),
       responsive: ["xs", "sm", "md"],
@@ -86,11 +127,11 @@ const ConductoresTable = () => {
     key: conductor.idConductor.toString(),
     imgp: conductor.usuario.fotoPerfil || defaultimg,
     name: conductor.usuario.nombre,
-    age: conductor.usuario.fechaNacimiento || "No especificada",
+    age: Math.floor((new Date() - new Date(conductor.usuario.fechaNacimiento)) / (365.25 * 24 * 60 * 60 * 1000)) || "No especificada",
     phone: conductor.usuario.numeroTelefono || "No especificado",
     email: conductor.usuario.correo,
-  }));
-
+  })
+  );
   return (
     <div style={{ overflowX: "auto" }}>
       <Link to="/conductoresRegister">
--- src/service/conductores/serviceConductores.js
+++ src/service/conductores/serviceConductores.js
@@ -22,4 +22,104 @@ export const getAllConductores= async () => {
   }
 };
 
+export const getConductorById = async (id) => {
+  try {
+    const response = await axios.get(
+      createURL([`/conductor/${id}`]),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        }
+      }
+    );
 
+    return response;
+
+  } catch (error) {
+    return error;
+  }
+};
+
+export const updateConductorById = async (nombre, rfc, foto, puntuacion, correo, clave, bday,phone, id, idUser) => {
+  try {
+    const response = await axios.post(
+      createURL(['/auth/conductor/']),
+      {
+        "idConductor": id,
+        "idConductorUser": idUser,
+        "nombre": nombre,
+        "rfc": rfc,
+        "fotoPerfil": null,
+        "puntuacion": puntuacion,
+        "correo": correo,
+        "clave": clave,
+        "fechaNacimiento":bday,
+        "numeroTelefono": phone,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        }
+      }
+    );
+
+    return response;
+
+  } catch (error) {
+    return error;
+  }
+};
+
+
+
+//Create
+export const saveOrUpdateConductor = async (nombre, rfc, foto, puntuacion, correo, clave, bday,phone) => {
+  try {
+    const response = await axios.post(
+      createURL(['/auth/conductor/']),
+      {
+        "nombre": nombre,
+        "rfc": rfc,
+        "fotoPerfil": null,
+        "puntuacion": 0,
+        "correo": correo,
+        "clave": clave,
+        "fechaNacimiento":bday,
+        "numeroTelefono": phone,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        }
+      }
+    );
+
+    return response;
+
+  } catch (error) {
+    return error;
+  }
+};
+
+
+export const deleteConductorById = async (id) => {
+  try {
+    const response = await axios.delete(
+      createURL([`/conductor/${id}`]),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        }
+      }
+    );
+
+    return response;
+
+  } catch (error) {
+    return error;
+  }
+};
